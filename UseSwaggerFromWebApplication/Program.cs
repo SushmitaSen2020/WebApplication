6448636846416725a64f70c902c1f29979afe49c
Sushmita Sen (QUANTUM LEAP CONSULTING PRIVAT) <v-susse@microsoft.com>
2020-12-10 11:11:47 +0530

    Add project files.

---
+++ UseSwaggerFromWebApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UseSwaggerFromWebApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WebApplication/Controllers/WeatherForecastController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Weather_Forecast_ExampleController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<Weather_Forecast_ExampleController> _logger;
+
+        public Weather_Forecast_ExampleController(ILogger<Weather_Forecast_ExampleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Get")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("GetById")]
+        public IEnumerable<WeatherForecast> GetById(int id)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+    }
+}
